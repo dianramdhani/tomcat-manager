632170410b8dbb5d5b98967728f0c7f6551c38ca
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-06-18 10:47:18 +0700

    update drl-widget2: add all params, dashboard: add all data to view

--- assets/components/drl-widget2/drl-widget2.js
+++ assets/components/drl-widget2/drl-widget2.js
@@ -7,15 +7,13 @@
     window.app
         .component('drlWidget2', {
             template: require('./drl-widget2.html'),
-            controller: drl,
             bindings: {
-                Binding: '=',
+                img: '@',   // Required. Type string. Img src.
+                text1: '@', // Required. Type string.
+                text2: '@', // Required. Type string.
+                text3: '@', // Required. Type string.
+                text4: '@', // Required. Type string.
+                menu: '='   // Required. Type object. Button menu properties.
             },
         });
-
-    drl.$inject = [];
-    function drl() {
-        let $ctrl = this;
-        $ctrl.$onInit = () => { };
-    }
 })();
--- assets/views/dashboard/dashboard.js
+++ assets/views/dashboard/dashboard.js
@@ -10,32 +10,48 @@
             controller: _
         });
 
-    _.$inject = ['$scope', 'AMQManagerService', 'ManagerService'];
-    function _($scope, AMQManagerService, ManagerService) {
+    _.$inject = ['$scope', '$timeout', 'AMQManagerService', 'ManagerService', 'UtilService'];
+    function _($scope, $timeout, AMQManagerService, ManagerService, UtilService) {
         let $ctrl = this;
-        $ctrl.$onInit = async () => {
-            /**
-             * Check health.
-             * @param {Array} arr Required. List data.
-             * @param {String} idName Required. Name of id.
-             * @param {Function} service Required. Service function.
-             */
-            const checkHealth = async (arr, idName, service) => {
-                for (const i in arr) {
-                    arr[i]['health'] = await service(arr[i][idName]).then(_ => _.data.object);
-                }
+        $ctrl.$onInit = () => {
+            const getInitialData = async () => {
+                /**
+                 * Check health.
+                 * @param {Array} arr Required. List data.
+                 * @param {String} idName Required. Name of id.
+                 * @param {Function} service Required. Service function.
+                 */
+                const checkHealth = async (arr, idName, service) => {
+                    for (const i in arr) {
+                        arr[i]['health'] = await service(arr[i][idName]).then(_ => _.data.object);
+                    }
+                };
+
+                // get all list
+                let listAmq = await AMQManagerService.listAmq(0, 12).then(_ => _.data.iteratorObject),
+                    listAgent = await ManagerService.listAgent(0, 12).then(_ => _.data.iteratorObject),
+                    listGroupInstance = await ManagerService.listGroupInstance(0, 10).then(_ => _.data.iteratorObject);
+
+                // check all health
+                await checkHealth(listAmq, 'instanceAmqId', AMQManagerService.checkAgentHealth);
+                await checkHealth(listAgent, 'agentId', ManagerService.checkAgentHealth);
+
+                return {
+                    listAmq,
+                    listAgent,
+                    listGroupInstance
+                };
             };
 
-            // get all list
-            $scope.listAmq = await AMQManagerService.listAmq(0, 12).then(_ => _.data.iteratorObject);
-            $scope.listAgent = await ManagerService.listAgent(0, 12).then(_ => _.data.iteratorObject);
-            $scope.listGroupInstance = await ManagerService.listGroupInstance(0, 10).then(_ => _.data.iteratorObject);
-
-            // check all health
-            await checkHealth($scope.listAmq, 'instanceAmqId', AMQManagerService.checkAgentHealth);
-            await checkHealth($scope.listAgent, 'agentId', ManagerService.checkAgentHealth);
-
-            console.log($scope.listAmq, $scope.listAgent, $scope.listGroupInstance);
+            UtilService.drlLoading(true);
+            getInitialData().then(({ listAmq, listAgent }) => {
+                $timeout(() => {
+                    $scope.listAmq = listAmq;
+                    $scope.listAgent = listAgent;
+                    UtilService.drlLoading(false);
+                });
+                console.log({ listAmq, listAgent });
+            });
         };
     }
 })();
