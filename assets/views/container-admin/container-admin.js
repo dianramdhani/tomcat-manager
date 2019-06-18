beb7fd308a7ca3f3eb3adf15686d2c807050389f
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-06-18 13:23:41 +0700

    update container-admin: add menu tomcat instance

--- assets/views/container-admin/container-admin.js
+++ assets/views/container-admin/container-admin.js
@@ -10,10 +10,18 @@
             controller: _
         });
 
-    _.$inject = ['$scope'];
-    function _($scope) {
+    _.$inject = ['$scope', 'ManagerService'];
+    function _($scope, ManagerService) {
         let $ctrl = this;
         $ctrl.$onInit = () => {
+            /**
+             * Get initial data.
+             */
+            const getInitialData = async () => {
+                let listAgent = await ManagerService.listAgent(0, 12).then(_ => _.data.iteratorObject);
+                return { listAgent };
+            }
+
             $scope.menu = {
                 sidebar: [
                     {
@@ -27,13 +35,9 @@
                         icon: 'icon ion-cube',
                         menu: [
                             {
-                                title: 'Tomcat 1',
-                                href: 'admin.tomcatInstance'
-                            },
-                            {
-                                title: 'Tomcat 2',
-                                href: 'admin.tomcatInstance'
-                            },
+                                title: 'Add Instance',
+                                href: ''
+                            }
                         ]
                     }
                 ],
@@ -45,6 +49,16 @@
                     }
                 ]
             };
+
+            getInitialData().then(({ listAgent }) => {
+                // add menu Tomcat Instance
+                listAgent.forEach(agent => {
+                    $scope.menu.sidebar.find(({ title }) => title === 'Tomcat Instance').menu.push({
+                        title: agent.agentName,
+                        href: 'admin.tomcatInstance'
+                    });
+                });
+            });
         };
     }
 })();
