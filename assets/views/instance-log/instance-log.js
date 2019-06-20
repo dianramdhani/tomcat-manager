7fef207c98f15ba6b8ab31e33a29e32b0aea2945
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-06-20 12:14:43 +0700

    update instace-log: add datatable

--- assets/views/instance-log/instance-log.js
+++ assets/views/instance-log/instance-log.js
@@ -9,8 +9,27 @@
     window.app
         .component('instanceLog', {
             template: require('./instance-log.html'),
+            controller: _,
             bindings: {
                 log: '=',
             }
         });
+
+    _.$inject = ['$timeout'];
+    function _($timeout) {
+        let $ctrl = this;
+        $ctrl.$onInit = () => {
+            $timeout(() => {
+                $('#log').DataTable({
+                    lengthMenu: [5, 10, 25, 50, 75, 100],
+                    language: {
+                        paginate: {
+                            previous: `<i class="fa fa-angle-left"></i>`,
+                            next: `<i class="fa fa-angle-right"></i>`
+                        }
+                    }
+                });
+            });
+        };
+    }
 })();
