43dd67578625fbc9492359dee5bd009793393a24
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-06-27 10:58:00 +0700

    update data-source: fix bug select2 cant ng-model

--- assets/views/data-source/data-source.js
+++ assets/views/data-source/data-source.js
@@ -10,8 +10,8 @@
             controller: _,
         });
 
-    _.$inject = ['$scope', '$q', 'DatasourceService', 'ManagerService'];
-    function _($scope, $q, DatasourceService, ManagerService) {
+    _.$inject = ['$scope', '$q', '$timeout', 'DatasourceService', 'ManagerService'];
+    function _($scope, $q, $timeout, DatasourceService, ManagerService) {
         let $ctrl = this;
         $ctrl.$onInit = async () => {
             /**
@@ -19,16 +19,36 @@
              */
             const getInitialData = async () => {
                 let datasources = await DatasourceService.getDatasourceByDatasourceId().then(_ => _.data.object),
-                    instancesTarget = await $q.all(datasources.map(datasource => DatasourceService.getDatasourceInstanceByDatasourceId(datasource.dataSourceId).then(_ => _.data.object)));
+                    instancesTarget = await $q.all(datasources.map(datasource => DatasourceService.getDatasourceInstanceByDatasourceId(datasource.dataSourceId).then(_ => _.data.object))),
+                    instances = await ManagerService.listAllInstances(0, 12).then(_ => _.data.object);
                 datasources.forEach((datasource, i) => {
-                    datasource['instancesTarget'] = instancesTarget[i];
+                    datasource['instancesTarget'] = instancesTarget[i].map(_ => _.instanceId);
                 });
-                console.log(datasources);
-                return [datasources];
+                console.log({ datasources, instances });
+                return [datasources, instances];
             };
 
-            [$scope.datasource] = await getInitialData();
+            [$scope.datasources, $scope.instances] = await getInitialData();
             $scope.$apply();
+
+            $timeout(() => {
+                // initial select2
+                for (const i in $scope.datasources) {
+                    angular.element(`#instances-target-${i}`).select2({ width: '100%' });
+                }
+            });
+        };
+
+        $scope.differ = (a, b) => {
+            return !angular.equals(a, b);
+        };
+
+        $scope.instancesTargetChanges = (datasource, instancesId) => {
+            let instancesTarget = instancesId.map((instanceId) => {
+                let instance = $scope.instances.filter(instance => instance.instanceId === instanceId)[0];
+                return instance;
+            });
+            console.log({ datasource, instancesTarget });
         };
     }
 })();
