9d5201164b7907650ccd4a8d8250e861f5e3dbcf
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-07-11 14:36:17 +0700

    update work-manager: add instances target

--- assets/views/work-manager/work-manager.js
+++ assets/views/work-manager/work-manager.js
@@ -10,28 +10,47 @@
             controller: _,
         });
 
-    _.$inject = ['$scope', '$q', 'WorkManagerService', 'ManagerService'];
-    function _($scope, $q, WorkManagerService, ManagerService) {
+    _.$inject = ['$scope', '$q', '$timeout', '$element', 'WorkManagerService', 'ManagerService'];
+    function _($scope, $q, $timeout, $element, WorkManagerService, ManagerService) {
+        /**
+        * Get initial data.
+        */
+        const getInitialData = async () => {
+            let listWorkManager = await WorkManagerService.listAllWorkManager().then(_ => _.data.object),
+                listWorkManagerInstances = await $q.all(listWorkManager.map(({ workManagerId }) => WorkManagerService.getWorkmanagerInstanceByWorkmanagerId(workManagerId).then(_ => _.data.object))),
+                instances = await ManagerService.listAllInstances(0, 12).then(_ => _.data.object);
+            listWorkManagerInstances.forEach((instancesTarget, i) => {
+                listWorkManager[i]['instancesTarget'] = instancesTarget.map(_ => _.instanceId);
+            });
+            console.log({ listWorkManager, instances });
+            return [listWorkManager, instances];
+        };
+
+        /**
+         * Refresh datasource dan instances.
+         */
+        const refreshData = async () => {
+            [$scope.listWorkManager, $scope.instances] = await getInitialData();
+            $scope.$apply();
+        }
+
         let $ctrl = this;
         $ctrl.$onInit = async () => {
-            /**
-             * Get initial data.
-             */
-            const getInitialData = async () => {
-                let listWorkManager = await WorkManagerService.listAllWorkManager().then(_ => _.data.object),
-                    listWorkManagerInstances = await $q.all(listWorkManager.map(({ workManagerId }) => WorkManagerService.getWorkmanagerInstanceByWorkmanagerId(workManagerId).then(_ => _.data.object))),
-                    instances = await ManagerService.listAllInstances(0, 12);
-                listWorkManagerInstances.forEach((instances, i) => {
-                    listWorkManager[i]['instances'] = instances.map(_ => _.instanceId);
-                });
-                // issue: fungsi instance harusnya yang belum sama sekali di gunakan oleh workManager.
-                // sehingga servicenya beda.
-                console.log({ listWorkManager, instances });
-                return [listWorkManager];
-            };
+            await refreshData();
+            $timeout(() => {
+                // initial select2
+                for (const i in $scope.listWorkManager) {
+                    $element.find(`#instances-target-${i}`).select2({ width: '100%' });
+                }
+            });
+        };
 
-            [$scope.listWorkManager] = await getInitialData();
-            $scope.$apply();
+        $scope.differ = (a, b) => {
+            return !angular.equals(a, b);
+        };
+
+        $scope.instancesTargetChange = (workManager, instancesTarget) => {
+            console.log('change instance target work manager', workManager, instancesTarget);
         };
     }
 })();
